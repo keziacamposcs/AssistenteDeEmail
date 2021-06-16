445891d20ca9b3f5178b4f81ac489c1d99e85e9a
Kezia Campos <keziacamposcs@gmail.com>
2021-06-16 09:09:22 -0300

    atualização 16.06.21

--- IAEmail.py
+++ IAEmail.py
@@ -1,24 +1,25 @@
 import speech_recognition as sr
 import yagmail
 
-recognizer = sr.Recognizer()
-with sr.Microphone() as souce:
-  print('Limpando o som externo...')
-  recognizer.adjust_for_ambient_noise(source,duration=1)
-  print("Aguardando sua mensagem...")
-  recordedaudio = recognizer.listen(source)
-  print('Gravação realizada!')
+recognizer= sr.Recognizer()
+with sr.Microphone() as source:
+    print('Limpando som externo...')
+    recognizer.adjust_for_ambient_noise(source,duration=1)
+    print("Aguardando sua mensagem...")
+    recordedaudio= recognizer.listen(source)
+    print('Gravação realizada..!')
 try:
-  print('Digitando sua mensagem...')
-  text = recognizer.recognize_google(recordeaudio, laguage ='pt-BR')
-  
-  print('Sua mensagem:{}'.format(text))
-  
+    print('Digitando mensagem...')
+    text= recognizer.recognize_google(recordedaudio,language='pt-BR')
+
+    print('Sua mensagem:{}'.format(text))
+
 except Exception as ex:
-  print(ex)
-  
-#Automatizando e-mail
-reciever = 'donacareta@gmail.com'
-message = text
-sender = yagmail.SMTP('keziacamposcs@gmail.com')
-sender.send(to=reciever, subject = 'Email automatizado por voz', contents = message)
+    print(ex)
+
+#Automatizando emails:
+
+reciever='email_do_destinário'
+message=text
+sender=yagmail.SMTP('email_do_remetente')
+sender.send(to=reciever,subject='Este é um e-mail automatizado by python',contents=message)
