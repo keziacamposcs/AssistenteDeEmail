366e6ec21c5c4a59edb015cecb0e0f0e25ae9877
Kezia Campos <keziacamposcs@gmail.com>
2021-06-16 11:34:59 -0300

    Update IAEmail.py

--- IAEmail.py
+++ IAEmail.py
@@ -6,31 +6,29 @@ listener = sr.Recognizer()
 
 engine = pyttsx3.init()
 engine.setProperty("rate", 178)
-engine.say("Oi, eu sou sua Assistente, como posso te ajudar?")
+engine.say("Oi, eu sou sua Assistente de E-mail, como posso te ajudar?")
 engine.say("Já estou te escutando")
-
 engine.runAndWait()
-
-
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
 
 recognizer= sr.Recognizer()
 with sr.Microphone() as source:
 
-    engine = pyttsx3.init()
-    
     engine.setProperty("rate", 178)
     engine.say("Limpando som externo...")
     engine.runAndWait()
     print('Limpando som externo...')
-
     recognizer.adjust_for_ambient_noise(source,duration=1)
+
+
+    engine.setProperty("rate", 178)
     engine.say("Aguardando sua mensagem...")
+    engine.runAndWait()
     print("Aguardando sua mensagem...")
     recordedaudio= recognizer.listen(source)
+
+    engine.setProperty("rate", 178)
     engine.say("Gravação realizada..!")
+    engine.runAndWait()
     print('Gravação realizada..!')
 try:
     print('Digitando mensagem...')
@@ -42,8 +40,7 @@ except Exception as ex:
     print(ex)
 
 #Automatizando emails:
-
-reciever='donacareta@gmail.com'
+reciever='email_do_destinario'
 message=text
-sender=yagmail.SMTP('keziacamposcs@gmail.com')
+sender=yagmail.SMTP('email_do_remetente')
 sender.send(to=reciever,subject='Este é um e-mail automatizado by python',contents=message)
